0704e40ec27bc890af6c75a208223eef0e64b42f
Michael <checomichael2@gmail.com>
2019-04-06 13:11:09 -0400

    changed header color

--- components/header.js
+++ components/header.js
@@ -1,9 +1,14 @@
 function Header() {
-  return (
-    <header>
-      <h1>Next.js Example on Now 2.0</h1>
-    </header>
-  );
+	return (
+		<header>
+			<h1>Next.js Example on Now 2.0</h1>
+			<style jsx>{`
+				h1 {
+					color: red;
+				}
+			`}</style>
+		</header>
+	);
 }
 
 export default Header;
